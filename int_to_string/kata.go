d7f110d56da5baf7dd5da404f79d1410989caef6
Sammi Turner <ms.sammi.turner@gmail.com>
2022-12-12 20:07:17 +0000

    Update kata.go

--- int_to_string/kata.go
+++ int_to_string/kata.go
@@ -1,9 +1,15 @@
-package kata
+package main
 
 import (
+    "fmt"
     "strconv"
 )
 
 func NumberToString(n int) string {
     return strconv.Itoa(n)
+}
+
+func main() {
+    r := NumberToString(29817)
+    fmt.Println(r)
 }
