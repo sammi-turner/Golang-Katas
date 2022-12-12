eb219b75eb9eda5f397f49fab561ef8d09281beb
Sammi Turner <ms.sammi.turner@gmail.com>
2022-12-12 20:11:52 +0000

    Update kata.go

--- even_or_odd/kata.go
+++ even_or_odd/kata.go
@@ -1,8 +1,14 @@
-package kata
+package main
+
+import "fmt"
 
 func EvenOrOdd(number int) string {
 	if number % 2 == 0 {
     	return "Even"
 	}
 	return "Odd"
+}
+
+func main() {
+	fmt.Println(EvenOrOdd(519))
 }
